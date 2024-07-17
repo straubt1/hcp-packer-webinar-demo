fdb5a88442a2a4bbf17f03a0813586d08e5f3c09
Tom Straub <tstraub@hashicorp.com>
2024-07-16 20:45:25 -0500

    Clean version print

--- hcp-terraform/aws/main.tf
+++ hcp-terraform/aws/main.tf
@@ -1,13 +1,18 @@
-data "hcp_packer_artifact" "hashicat-image" {
+locals {
   bucket_name  = "hashicat-ubuntu-2204"
   channel_name = "release"
+}
+
+data "hcp_packer_artifact" "hashicat-image" {
+  bucket_name  = local.bucket_name
+  channel_name = local.channel_name
   platform     = "aws"
   region       = var.region
 }
 
 data "hcp_packer_version" "hashicat-image" {
-  bucket_name  = "hashicat-ubuntu-2204"
-  channel_name = "release"
+  bucket_name  = local.bucket_name
+  channel_name = local.channel_name
 }
 
 module "hashicat-aws" {
--- hcp-terraform/aws/outputs.tf
+++ hcp-terraform/aws/outputs.tf
@@ -2,13 +2,6 @@ output "url" {
   value = module.hashicat-aws.catapp_url
 }
 
-output "image-created-at" {
-  value = data.hcp_packer_artifact.hashicat-image.created_at
-}
-
-output "debug"{
-  value = {
-    a = data.hcp_packer_artifact.hashicat-image
-    b = data.hcp_packer_version.hashicat-image
-  }
+output "image-version" {
+  value = data.hcp_packer_version.hashicat-image.name
 }
