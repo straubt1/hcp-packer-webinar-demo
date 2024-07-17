ac1b15db601fa9b07d3a51b9ff7341502affb84a
Tom Straub <tstraub@hashicorp.com>
2024-07-17 11:50:25 -0500

    fixed output

--- hcp-terraform/gcp/gcp-compute/outputs.tf
+++ hcp-terraform/gcp/gcp-compute/outputs.tf
@@ -1,8 +1,8 @@
 # Outputs file
 output "catapp_url" {
-  value = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
+  value = "http://${google_compute_instance.default.network_interface[0].access_config[0].nat_ip}"
 }
 
 output "catapp_ip" {
-  value = google_compute_instance.default.network_interface[0].access_config[0].public_ptr_domain_name
+  value = google_compute_instance.default.network_interface[0].access_config[0].nat_ip
 }
